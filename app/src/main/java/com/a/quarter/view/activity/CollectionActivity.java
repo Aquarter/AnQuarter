bf8e9b0c2fdd3be7b964c8e82eccbaa92bb5ed0c
Wangpengzhi666 <983885385@qq.com>
2017-08-01 10:03:54 +0800

    工具栏

--- app/src/main/java/com/a/quarter/view/activity/CollectionActivity.java
+++ app/src/main/java/com/a/quarter/view/activity/CollectionActivity.java
@@ -23,6 +23,7 @@ public class CollectionActivity extends BaseActivity<CollectionPresenter> implem
 
     private TextView toolbar_title;
     private LinearLayout toolbar_back;
+    private TextView toolbar_btn;
 
     @Override
     public void onsuccess(Object o) {
@@ -48,20 +49,26 @@ public class CollectionActivity extends BaseActivity<CollectionPresenter> implem
     protected void initView() {
         toolbar_title = (TextView) findViewById(R.id.toolbar_title);//标题头
         toolbar_back = (LinearLayout) findViewById(R.id.toolbar_back);//返回按钮
+        toolbar_btn = (TextView) findViewById(R.id.toolbar_btn);//右边的TextView
 
 
         toolbar_back.setOnClickListener(this);
+        toolbar_btn.setOnClickListener(this);
     }
 
     @Override
     protected void initDatas() {
         toolbar_title.setText("我的收藏");
+        toolbar_btn.setText("删除");
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.toolbar_back:
+                this.finish();
+                break;
+            case R.id.toolbar_btn:
                 this.finish();
                 break;
         }
